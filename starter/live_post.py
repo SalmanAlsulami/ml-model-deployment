0fccf1390b67d8332a6639536094df473be9956e
SalmanAlsulami <s444002538@st.uqu.edu.sa>
2026-03-25 13:10:27 +0300

    Add live_post script and screenshots

---
+++ starter/live_post.py
@@ -0,0 +1,24 @@
+import requests
+
+url = "https://ml-model-deployment-cq7q.onrender.com/predict"
+
+data = {
+    "age": 39,
+    "workclass": "State-gov",
+    "fnlgt": 77516,
+    "education": "Bachelors",
+    "education-num": 13,
+    "marital-status": "Never-married",
+    "occupation": "Adm-clerical",
+    "relationship": "Not-in-family",
+    "race": "White",
+    "sex": "Male",
+    "capital-gain": 2174,
+    "capital-loss": 0,
+    "hours-per-week": 40,
+    "native-country": "United-States"
+}
+
+response = requests.post(url, json=data)
+print(f"Status Code: {response.status_code}")
+print(f"Response: {response.json()}")
